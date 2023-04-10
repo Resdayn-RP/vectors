57befac34c745e5f30c65f5e8652de68af1f1add
HebiKotei <c1yvan@outlook.com>
2023-04-10 22:43:20 +0100

    tweak: removed useless new function

--- main.lua
+++ main.lua
@@ -8,24 +8,24 @@ function vec3:__call(x, y, z)
 end
 
 function vec3.__add(a, b)
-    return vec3:new(a.x + b.x, a.y + b.y, a.z + b.z)
+    return vec3(a.x + b.x, a.y + b.y, a.z + b.z)
 end
 
 function vec3.__sub(a, b)
-    return vec3:new(a.x - b.x, a.y - b.y, a.z - b.z)
+    return vec3(a.x - b.x, a.y - b.y, a.z - b.z)
 end
 
 function vec3.__mul(a, b)
     if type(a) == 'number' then 
-        return vec3:new(a * b.x, a * b.y, a * b.z)
+        return vec3(a * b.x, a * b.y, a * b.z)
     elseif type(b) == 'number' then
-        return vec3:new(a.x * b, a.y * b, a.z*b)
+        return vec3(a.x * b, a.y * b, a.z*b)
     end
-    return vec3:new(a.x*b.x, a.y*b.y, a.z*b.z)
+    return vec3(a.x*b.x, a.y*b.y, a.z*b.z)
 end
 
 function vec3.__div(a, b)
-    return vec3:new(a.x/b, a.y/b, a.z/b)
+    return vec3(a.x/b, a.y/b, a.z/b)
 end
 
 function vec3.__len(a)
@@ -38,10 +38,6 @@ end
 
 function vec3.__tostring(a)
     return "vec3(" .. a.x .. ", " .. a.y .. ', ' .. a.z .. ")"
-end
-
-function vec3:new(x, y, z)
-    return setmetatable({x=x or 0,y=y or 0,z=z or 0}, getmetatable(self))
 end
 
 return setmetatable(vec3, vec3)
