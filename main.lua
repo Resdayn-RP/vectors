24761a109f9f1e2aa5f958475de33de6f5d365a0
Yvan Cywan <c1yvan@outlook.com>
2023-04-19 11:45:50 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,10 +3,6 @@ local vec3 = {}
 
 vec3.__index = vec3
 
-function vec3:__typeof()
-    return "Vector3"
-end
-
 function vec3:__call(x, y, z)
     return setmetatable({x=x or 0,y=y or 0,z=z or 0}, getmetatable(self))
 end
